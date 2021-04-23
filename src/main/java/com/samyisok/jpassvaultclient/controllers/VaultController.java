f365ada8d07908240226dc2eb0d7f52ebda88ec8
Sergey M <magaster@gmail.com>
2021-04-23 16:51:03 +0300

    Add Vault

--- src/main/java/com/samyisok/jpassvaultclient/controllers/VaultController.java
+++ src/main/java/com/samyisok/jpassvaultclient/controllers/VaultController.java
@@ -3,8 +3,10 @@ package com.samyisok.jpassvaultclient.controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 import com.samyisok.jpassvaultclient.StageActionEvent;
 import com.samyisok.jpassvaultclient.StageActionEvent.Payload;
+import com.samyisok.jpassvaultclient.models.Vault;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
@@ -23,10 +25,8 @@ public class VaultController implements Initializable {
   @Autowired
   private ApplicationContext appContext;
 
-  ObservableList<String> vaultElems = FXCollections.observableArrayList(
-      "pornhub", "MAL", "animechart", "bash.org.ru", "yandex", "google", "pornhub",
-      "MAL", "animechart", "bash.org.ru", "yandex", "google", "pornhub", "MAL",
-      "animechart", "bash.org.ru", "yandex", "google");
+  @Autowired
+  Vault vault;
 
   @FXML
   ListView<String> listVault = new ListView<String>();
@@ -41,6 +41,8 @@ public class VaultController implements Initializable {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
+    ObservableList<String> vaultElems =
+        FXCollections.observableArrayList(vault.keySet());
     listVault.getItems().addAll(vaultElems);
   }
 }
---
+++ src/main/java/com/samyisok/jpassvaultclient/models/VaultContainer.java
@@ -0,0 +1,97 @@
+package com.samyisok.jpassvaultclient.models;
+
+public class VaultContainer {
+  String password;
+  String login;
+
+  /**
+   * @param password
+   * @param login
+   */
+  public VaultContainer(String password, String login) {
+    this.password = password;
+    this.login = login;
+  }
+
+  /**
+   * @return the password
+   */
+  public String getPassword() {
+    return password;
+  }
+
+  /**
+   * @param password the password to set
+   */
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  /**
+   * @return the login
+   */
+  public String getLogin() {
+    return login;
+  }
+
+  /**
+   * @param login the login to set
+   */
+  public void setLogin(String login) {
+    this.login = login;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#hashCode()
+   */
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((login == null) ? 0 : login.hashCode());
+    result = prime * result + ((password == null) ? 0 : password.hashCode());
+    return result;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (!(obj instanceof VaultContainer))
+      return false;
+    VaultContainer other = (VaultContainer) obj;
+    if (login == null) {
+      if (other.login != null)
+        return false;
+    } else if (!login.equals(other.login))
+      return false;
+    if (password == null) {
+      if (other.password != null)
+        return false;
+    } else if (!password.equals(other.password))
+      return false;
+    return true;
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#toString()
+   */
+
+  @Override
+  public String toString() {
+    return "VaultContainer [login=" + login + ", password=" + password + "]";
+  }
+
+
+}
